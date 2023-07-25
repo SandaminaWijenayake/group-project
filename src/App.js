a8cc441f2c7785a85e51b15a289ceee56a98892d
supem5 <136429127+supem5@users.noreply.github.com>
2023-07-25 22:07:19 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import "./App.css";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import AdsContent from "./Components/AdsContent";
-import NavBar from "./Components/NavBar";
-import GlobeStateProvider from "./GlobleState/GlobleState";
-import Login from "./Components/Login";
-import Signup from "./Components/Signup";
-import UserCardFull from "./Components/UserCardFull";
+import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import AdsContent from './Components/AdsContent';
+import NavBar from './Components/NavBar';
+import GlobeStateProvider from './GlobleState/GlobleState';
+import Login from './Components/Login';
+import Signup from './Components/Signup';
+import UserCardFull from './Components/UserCardFull';
+import CoverPage from './Components/coverpage';
 
 function App() {
   return (
     <GlobeStateProvider>
       <Router>
+        <NavBar />
         <div className="App">
-          <NavBar />
           <Routes>
-            <Route path="/" element={<AdsContent />} />
+            <Route path="/" element={<CoverPage />} />
+            <Route path="/hp" element={<AdsContent />} />
             <Route path="/Login" element={<Login />} />
             <Route path="/Signup" element={<Signup />} />
-            <Route path={"/Card/"} element={<UserCardFull />} />
+            <Route path={'/Card/'} element={<UserCardFull />} />
           </Routes>
         </div>
       </Router>
