d8c882388434f57ada77f97d6412efc0f422b126
SandaminaWijenayake <sandaminawijenayake0717@gmail.com>
2023-08-01 21:55:35 +0530

    fix

--- src/Components/AdsContent.js
+++ src/Components/AdsContent.js
@@ -36,17 +36,17 @@ const AdsContent = () => {
       });
   }, []);
 
-  // var Tawk_API = Tawk_API || {};
+  var Tawk_API = Tawk_API || {};
 
-  // (function () {
-  //   var s1 = document.createElement("script"),
-  //     s0 = document.getElementsByTagName("script")[0];
-  //   s1.async = true;
-  //   s1.src = "https://embed.tawk.to/64c58aa494cf5d49dc673ab0/1h6hqb5lj";
-  //   s1.charset = "UTF-8";
-  //   s1.setAttribute("crossorigin", "*");
-  //   s0.parentNode.insertBefore(s1, s0);
-  // })();
+  (function () {
+    var s1 = document.createElement("script"),
+      s0 = document.getElementsByTagName("script")[0];
+    s1.async = true;
+    s1.src = "https://embed.tawk.to/64c58aa494cf5d49dc673ab0/1h6hqb5lj";
+    s1.charset = "UTF-8";
+    s1.setAttribute("crossorigin", "*");
+    s0.parentNode.insertBefore(s1, s0);
+  })();
 
   return (
     <SectionCard className="mainContainer">
