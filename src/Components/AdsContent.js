a62735a99ce40759178452e2d70680c9a99d2591
SandaminaWijenayake <125541926+SandaminaWijenayake@users.noreply.github.com>
2023-08-09 02:03:14 +0530

    filterData

--- src/Components/AdsContent.js
+++ src/Components/AdsContent.js
@@ -20,16 +20,19 @@ import Checkbox from "@mui/material/Checkbox";
 
 const top100Films = [
   {
-    label: "20 - 25",
+    label: "20 - 24",
   },
   {
-    label: "25 - 30",
+    label: "25 - 29",
   },
   {
-    label: "30 - 35",
+    label: "30 - 34",
   },
   {
-    label: "35 - 40",
+    label: "35 - 39",
+  },
+  {
+    label: "40 - 44",
   },
   {
     label: "45 - 50",
@@ -56,8 +59,15 @@ const options = [
 
 const AdsContent = () => {
   const [data, setData] = useState([]);
+  const [initialState, setInitialState] = useState([]);
   const [manColor, setManColor] = useState(null);
   const [womanColor, setWomanColor] = useState(null);
+  const [currentState, setCurrentState] = useState({});
+
+  const [selectAge, setSelectAge] = useState(null);
+
+  const [selectGender, setSelectGender] = useState(null);
+  // const [change, setChange] = useState();
 
   const manIcon = {
     cursor: "pointer",
@@ -70,6 +80,7 @@ const AdsContent = () => {
 
   const womanIcon = {
     cursor: "pointer",
+    border: "none",
     // ":hover": {
     //   color: "rgb(125, 125, 125)",
     // },
@@ -87,7 +98,8 @@ const AdsContent = () => {
           usersdata.push({ ...doc.data(), id: doc.id });
         });
 
-        return setData(usersdata);
+        setData(usersdata);
+        setInitialState(usersdata);
       })
       .catch((err) => {
         console.log(err);
@@ -95,37 +107,114 @@ const AdsContent = () => {
   }, []);
   console.log(data);
 
-  const lookingForMan = async () => {
+  const handleFilter = () => {
+    let updatedList = initialState;
+    console.log(updatedList);
+
+    if (selectAge) {
+      console.log("select age: " + selectAge);
+      // updatedList = updatedList.filter((item) => item.age === selectAge);
+      updatedList = selectAge;
+      console.log("updated list :" + updatedList);
+    }
+
+    if (selectGender) {
+      console.log("selectGender :" + selectGender);
+
+      updatedList = updatedList.filter(
+        (item) => item.gender.label === selectGender
+      );
+    }
+    // let final = selectAge?.concat(selectGender);
+    // console.log("final : " + final);
+    // let uniqueChars = [...new Set(final)];
+    // console.log("new final : " + uniqueChars);
+    // setData(uniqueChars);
+    setData(updatedList);
+  };
+
+  const lookingForMan = () => {
     setWomanColor(false);
     setManColor(true);
+    setSelectGender("Male");
     // const colRef = collection(db, "users");
-    const colRef = query(collection(db, "users"));
+    // const colRef = query(collection(db, "users"));
 
-    const q = query(colRef, where("gender.label", "==", "Male"));
-    let userData = [];
-    const querySnapshot = await getDocs(q);
-    querySnapshot.forEach((doc) => {
-      // doc.data() is never undefined for query doc snapshots
+    // const q = query(colRef, where("gender.label", "==", "Male"));
+    // let userData = [];
+    // const querySnapshot = await getDocs(q);
+    // querySnapshot.forEach((doc) => {
+    //   // doc.data() is never undefined for query doc snapshots
 
-      userData.push({ id: doc.id, ...doc.data() });
-    });
-    return setData(userData);
+    //   userData.push({ id: doc.id, ...doc.data() });
+    // });
+    // return setData(userData);
+    // setSelectGender(
+    //   initialState.filter((item) => item.gender.label === "Male")
+    // );
   };
-  console.log(data);
+  // console.log(data);
 
-  const lookingForWoman = async () => {
+  const lookingForWoman = () => {
     setManColor(false);
     setWomanColor(true);
-    const colRef = collection(db, "users");
-    const q = query(colRef, where("gender.label", "==", "Female"));
-    // console.log(q);
-    let userData = [];
-    const querySnapshot = await getDocs(q);
-    querySnapshot.forEach((doc) => {
-      // doc.data() is never undefined for query doc snapshots
-      userData.push({ id: doc.id, ...doc.data() });
-    });
-    return setData(userData);
+    setSelectGender("Female");
+    // const colRef = collection(db, "users");
+    // const q = query(colRef, where("gender.label", "==", "Female"));
+    // // console.log(q);
+    // let userData = [];
+    // const querySnapshot = await getDocs(q);
+    // querySnapshot.forEach((doc) => {
+    //   // doc.data() is never undefined for query doc snapshots
+    //   userData.push({ id: doc.id, ...doc.data() });
+    // });
+    // return setData(userData);
+    // setSelectGender(
+    //   initialState.filter((item) => item.gender.label === "Female")
+    // );
+  };
+
+  const ageHander = (value) => {
+    // console.log(value);
+    if (value?.label === "20 - 24") {
+      setSelectAge(
+        initialState.filter((item) => item.age >= "20" && item.age < "25")
+      );
+    }
+    if (value?.label === "25 - 29") {
+      setSelectAge(
+        initialState.filter((item) => item.age >= "25" && item.age < "30")
+      );
+    }
+    if (value?.label === "30 - 34") {
+      setSelectAge(
+        initialState.filter((item) => item.age >= "30" && item.age < "35")
+      );
+    }
+    if (value?.label === "35 - 39") {
+      setSelectAge(
+        initialState.filter((item) => item.age >= "35" && item.age < "40")
+      );
+    }
+    if (value?.label === "40 - 44") {
+      setSelectAge(
+        initialState.filter((item) => item.age >= "40" && item.age < "45")
+      );
+    }
+    if (value?.label === "45 - 50") {
+      setSelectAge(
+        initialState.filter((item) => item.age >= "45" && item.age <= "50")
+      );
+    }
+  };
+
+  useEffect(() => {
+    console.log("ran");
+    handleFilter();
+  }, [selectAge, selectGender]);
+
+  const ethnicityHandler = (value) => {
+    console.log(value);
   };
 
   var Tawk_API = Tawk_API || {},
@@ -202,6 +291,10 @@ const AdsContent = () => {
                 sx={{ width: 200 }}
                 size="small"
                 renderInput={(params) => <TextField {...params} label="Age" />}
+                onChange={(e, value) => {
+                  ageHander(value);
+                  // setSelectAge(value.label);
+                }}
               />
             </Typography>
           </AccordionDetails>
@@ -216,11 +309,12 @@ const AdsContent = () => {
             <Typography>Ethnicity</Typography>
           </AccordionSummary>
           <AccordionDetails>
-            <FormGroup>
-              <FormControlLabel
-                control={<Checkbox defaultChecked />}
-                label="Sinhalese"
-              />
+            <FormGroup
+              onChange={(value) => {
+                ethnicityHandler(value);
+              }}
+            >
+              <FormControlLabel control={<Checkbox />} label="Sinhalese" />
               <FormControlLabel label="Tamils" control={<Checkbox />} />
               <FormControlLabel control={<Checkbox />} label="Burgher" />
               <FormControlLabel control={<Checkbox />} label="Malays" />
