71bf2ab0030fafc53a855ee08490588ee116f7b0
SandaminaWijenayake <125541926+SandaminaWijenayake@users.noreply.github.com>
2023-08-09 23:26:38 +0530

    fix

--- src/Components/Layout.js
+++ src/Components/Layout.js
@@ -1,8 +1,14 @@
-import { Outlet } from "react-router-dom";
+import { Navigate, Outlet } from "react-router-dom";
 import NavBar from "./NavBar";
 import Footer from "../UI/Footer";
+import { useContext } from "react";
+import { GlobleContext } from "../GlobleState/GlobleState";
 
 const Layout = () => {
+  const { isSignedIn } = useContext(GlobleContext);
+  if (!isSignedIn) {
+    return <Navigate to="/Login" replace />;
+  }
   return (
     <>
       <NavBar />
